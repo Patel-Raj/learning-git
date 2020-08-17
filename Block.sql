e67e2cfca69411aedc15c7a1fd3c3cb96e730a58
Raj Patel <patelraj22777@gmail.com>
2020-08-17 18:06:43 +0530

    removed unused select query

--- Block.sql
+++ Block.sql
@@ -10,7 +10,6 @@ begin
          dbms_output.put_line('else');
     end if;
     --l_temp_number := l_temp_number/0;
-    --select * from fnd_lookup_values where lookup_type = 'HRX_US_FEDERAL_FILING_STATUS';
 exception
     when zero_divide then
         dbms_output.put_line('abcd');
