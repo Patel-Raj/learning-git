e5cfe55da72e7dd5de1952d1eadd70b20904f41b
Raj Patel <patelraj22777@gmail.com>
2020-08-17 17:45:56 +0530

    Initial commit

---
+++ Block.sql
@@ -0,0 +1,18 @@
+set serveroutput on;
+declare
+    l_temp_number number := 10;
+    --l_temp_string varchar2(2); 
+begin
+    --l_temp_string := 'abc';
+    if true then
+     dbms_output.put_line('if');
+    else 
+         dbms_output.put_line('else');
+    end if;
+    --l_temp_number := l_temp_number/0;
+    --select * from fnd_lookup_values where lookup_type = 'HRX_US_FEDERAL_FILING_STATUS';
+exception
+    when zero_divide then
+        dbms_output.put_line('abcd');
+end;
+ 
