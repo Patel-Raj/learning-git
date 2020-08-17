59598c67ee2f8e2528ed94ac612356f23d8838ab
Raj Patel <patelraj22777@gmail.com>
2020-08-17 17:54:52 +0530

    Uncommented a varaiable

--- Block.sql
+++ Block.sql
@@ -1,7 +1,7 @@
 set serveroutput on;
 declare
     l_temp_number number := 10;
-    --l_temp_string varchar2(2); 
+    l_temp_string varchar2(2); 
 begin
     --l_temp_string := 'abc';
     if true then
