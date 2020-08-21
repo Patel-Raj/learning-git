371910526f20159f9c3a04e47df221f18178b110
Raj Patel <patelraj22777@gmail.com>
2020-08-21 09:09:18 +0530

    pl/sql case stmt

--- Block.sql
+++ Block.sql
@@ -2,14 +2,38 @@ set serveroutput on;
 declare
     l_temp_number number := 10;
     l_temp_string varchar2(2); 
+    l_grade char(1);
 begin
     --l_temp_string := 'abc';
     if true then
-     dbms_output.put_line('if');
+        dbms_output.put_line('if');
     else 
-         dbms_output.put_line('else');
+        dbms_output.put_line('else');
     end if;
     --l_temp_number := l_temp_number/0;
+    -- Case stmts
+    l_grade := 'D';
+    case l_grade
+    when 'A' then
+        dbms_output.put_line('Excellent');
+    when 'B' then
+        dbms_output.put_line('Very good');
+    when 'C' then
+        dbms_output.put_line('Good');
+    else 
+        dbms_output.put_line('Poor');
+    end case;
+    
+    -- Searched case stmt
+    case 
+    when l_temp_number > 10 then
+        dbms_output.put_line('Greater then 10');
+    when l_temp_number = 10 then
+        dbms_output.put_line('Equal 10');
+    else
+        dbms_output.put_line('Less then 10');
+    end case;
+    
 exception
     when zero_divide then
         dbms_output.put_line('abcd');
